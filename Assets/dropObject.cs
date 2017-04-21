dde9d7a299b02b2c8513f0d08f72e5bd4f4674e2
KevinVlietstra <kjvlietstra@gmail.com>
2017-04-21 14:46:48 +0200

    pushIt
    pushItRealGood

---
+++ Assets/dropObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dropObject : MonoBehaviour {
+
+	public GameObject obj;
+	public GameObject botBase;
+	public GameObject botTop;
+	public GameObject botCannon;
+	private int n;
+
+	void OnTriggerEnter(Collider coll)
+	{
+		if(coll.tag == "obj")
+		{
+			Debug.Log ("Enter!");
+			Destroy (coll.gameObject);
+			if (coll.gameObject.name == "bot_bot") {
+				botBase.SetActive (true);
+			}
+			if (coll.gameObject.name == "bot_bot (1)") {
+				botTop.SetActive (true);
+			}
+			if (coll.gameObject.name == "bot_barrel") {
+				botCannon.SetActive (true);
+			}
+				
+		}
+
+	}
+
+
+}
---
+++ Assets/pickUpObject.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pickUpObject : MonoBehaviour {
+	public GameObject obj;
+	public GameObject obj2;
+	public GameObject drpp;
+	private int n;
+
+	void OnTriggerEnter(Collider coll)
+	{
+		if(coll.tag == "obj")
+		{
+			Debug.Log ("Enter!");
+			coll.transform.parent = transform;
+			coll.transform.localScale = new Vector3 (200, 11, 200);
+			coll.transform.rotation = transform.parent.rotation;
+			n = 1;
+		}
+
+		Debug.Log (coll.tag);
+		if(coll.tag == "drpp")
+		{
+			Debug.Log ("Aangeraakt!");
+//			obj.transform.parent = coll.transform;
+//			obj.transform.localScale = new Vector3 (14, 141, 20);
+//			obj2.transform.parent = coll.transform;
+//			obj2.transform.localScale = new Vector3 (14, 141, 20);
+			n = 2;
+			StopCoroutine ("OnTriggerEnter");
+		}
+	}
+		
+
+	void Update()
+	{
+		if (n == 1)
+		{
+			Vector3 pos = obj.transform.position;
+			pos = Vector3.Lerp(obj.transform.position, transform.position, Time.deltaTime * 30);
+			obj.transform.position = pos;
+			obj.transform.rotation = transform.rotation;
+		}
+		if (n == 2)
+		{
+			Vector3 pos = obj.transform.position;
+			pos = Vector3.Lerp(obj.transform.position, drpp.transform.position, Time.deltaTime * 30);
+			obj.transform.position = pos;
+			obj.transform.rotation = drpp.transform.rotation;
+		}
+	}
+}
